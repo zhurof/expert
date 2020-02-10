26969a4fdc7930ce522117b37f95cba6ff5619d5
zhurof <zhuroff92@gmail.com>
2020-02-10 15:28:29 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+﻿function setCssVariables(){
+	document.documentElement.style.setProperty('--w-height',window.innerHeight + 'px');
+	document.documentElement.style.setProperty('--w-width',document.body.clientWidth + 'px');
+}
+setCssVariables();
+$(window).resize(setCssVariables);
